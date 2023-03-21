45268e1c30061d235a96dfbb9df93b6123b9b14b
bnchk <adarules@protonmail.com>
2023-03-21 13:00:15 +1100

    testnet code corrected

--- cardano-spo-setup.sh
+++ cardano-spo-setup.sh
@@ -27,14 +27,16 @@ cardano-cli address build \
     --payment-verification-key-file payment.vkey \
     --stake-verification-key-file stake.vkey \
     --out-file payment.addr \
-    --mainnetxxxFAILwanttest
+    --testnet-magic $CARDANO_NODE_MAGIC
 
 # Step 1d - Stake address generation (only for where protocol rewards are sent automatically)
 cardano-cli stake-address build \
     --stake-verification-key-file stake.vkey \
     --out-file stake.addr \
-    --mainnetxxxFAIL-testnet
+    --testnet-magic $CARDANO_NODE_MAGIC
 
+# Step 1e - Use faucet to load funds into the payment address (cat payment.addr)
+curl -XPOST "http[s]://$FQDN:$PORT/send-money/$ADDRESS"
 
 
 ###################################################################################################
@@ -43,3 +45,12 @@ cardano-cli stake-address build \
 #        - Create the transaction to submit the certificate to the blockchain
 #        - Sign the transaction
 #        - Submit the transaction
+
+# Step 2a - Registration Certificate creation
+cardano-cli stake-address registration-certificate \
+    --stake-verification-key-file stake.vkey \
+    --out-file stake.cert
+
+# Step 2b - Create Transaction (to be used to submit the certificate)
+#         - requires -> finding the UTXO (hash and tx_id) to be used for the payment
+#         - requires -> finding the slot number the blockchain tip is currently at
