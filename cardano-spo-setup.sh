e12b28e7ce9550d41adc3213627556e13977fc3b
bnchk <adarules@protonmail.com>
2023-03-21 12:38:10 +1100

    step 1 creating key pairs and addresses

--- cardano-spo-setup.sh
+++ cardano-spo-setup.sh
@@ -1,11 +1,45 @@
 #!/usr/bin/bash
-###########################
-#
-#  SPO SETUP COMMANDS
-#
-###########################
+################################
+#                              #
+#  CARDANO SPO SETUP COMMANDS  #
+#                              #
+################################
 
-# step 1 - Test to see if this makes it into Demeter.run
-# step 2 - how to get it back??
-# step 3 - now under WSL on W10 linked by VScode addon
+###################################################################################################
+# STEP 1 - CREATE REQUIRED KEY PAIRS AND ADDRESSES
+#        - There are 2 key pairs to create, payment and stake 
+#        - These are then used to generate matching addresses
+#           https://developers.cardano.org/docs/stake-pool-course/handbook/create-stake-pool-keys/
 
+# STEP 1a - Payment Key creation - same as for a wallet - payment key and secret signing key
+cardano-cli address key-gen \
+    --verification-key-file payment.vkey \
+    --signing-key-file payment.skey
+
+
+# Step 1b - Stake key pair generation
+cardano-cli stake-address key-gen \
+    --verification-key-file stake.vkey \
+    --signing-key-file stake.skey
+
+# Step 1c - Payment address generation (uses both the payment and stake public verification keys)
+cardano-cli address build \
+    --payment-verification-key-file payment.vkey \
+    --stake-verification-key-file stake.vkey \
+    --out-file payment.addr \
+    --mainnetxxxFAILwanttest
+
+# Step 1d - Stake address generation (only for where protocol rewards are sent automatically)
+cardano-cli stake-address build \
+    --stake-verification-key-file stake.vkey \
+    --out-file stake.addr \
+    --mainnetxxxFAIL-testnet
+
+
+
+###################################################################################################
+# STEP 2 - REGISTER STAKE CERTIFICATE WITH THE BLOCKCHAIN
+#        - Create the Registration Certificate for the stake pool
+#        - Create the transaction to submit the certificate to the blockchain
+#        - Sign the transaction
+#        - Submit the transaction
